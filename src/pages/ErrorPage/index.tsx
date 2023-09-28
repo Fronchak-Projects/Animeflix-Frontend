d9fb5cee7bfd09746d388a12c603cb3ecf800bf6
Gabriel Fronchak Gmack <gabrielfronchak@yahoo.com>
2023-09-28 19:35:14 -0300

    Fix error page

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,15 +1,17 @@
 import ErrorComponent from "../../components/ErrorComponent";
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/footer";
 
 const ErrorPage = () => {
 
   console.log('catch by ERROR PAGE!');
 
   return (
-    <>
+    <div className="d-flex flex-column justify-content-between min-vh-100">
       <Navbar />
       <ErrorComponent />
-    </>
+      <Footer />
+    </div>
   );
 }
 
